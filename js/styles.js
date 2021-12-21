5d9068fe0297e15e7c588dafcbfb05485e8e01cd
AntonioPtnR <Antonio.Paton2@alu.uclm.es>
2021-12-21 20:24:48 +0100

    Actualizacion en los apartados de la web

--- js/styles.js
+++
@@ -1,8 +0,0 @@
-document.onscroll = function() {
-    if( $(window).scrollTop() > $('nav').height() ) {
-        $('nav > div.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
-    }
-    else {
-        $('nav > div.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
-    }
-};
